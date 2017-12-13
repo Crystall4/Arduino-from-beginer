928d355bd74f6b3eb4148314c648c9a1f87e1422
Crystall4 <34477856+Crystall4@users.noreply.github.com>
2017-12-13 15:33:31 +0300

    Update first.ino
    Добавлена led_count_Pulse()

--- first.ino
+++ first.ino
@@ -4,6 +4,7 @@
 #define Pre_ON_pulse_time 1000     //Когда светодиод выключен и кнопка нажата
 #define Post_ON_pulse_time 500    //время моргания когда кнопка нажата и диод горит
 #define Pre_OFF_pulse_time 500   //время моргания когда отпустили кнопку, должен 10 раз моргнуть и выключиться
+#define Pre_OFF_pulse_count 10  //количество вспышек перед выключением 
 
 #define PRESS HIGH   //нажата
 #define FREE   LOW  //отпущена
@@ -16,11 +17,21 @@ void setup() {
   pinMode(Button_pin, INPUT);
 }
 
-void ledPulse(unsigned long pulse_time){        //процедура мигания, параметр период мигания
+void led_Pulse(unsigned long pulse_time){        //процедура мигания, параметр период мигания
   static unsigned long previousTime = 0;        //переменная для хранения времени последнего переключения
   if(millis() - previousTime > pulse_time) {    //если с последнего переключения прошло больше времени чем передано в параметре:
       previousTime = millis();                  //сохраняем текущее время как время последнего переключения    
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  //и реверсируем состояние светодиода
+  }
+}
+
+void led_count_Pulse(unsigned long pulse_count_time, unsigned int pulse_count){   //процедура мигания n раз 
+  static unsigned long previousTime = 0;                    //переменная для хранения времени последнего переключения
+  static unsigned int previousCount = 0;                    //переменная для хранения выполненных переключений
+  if(((millis() - previousTime) > pulse_count_time) && (previousCount < (pulse_count * 2))){ 
+	  previousCount = previousCount + 1;
+	  previousTime = millis();                  
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); 
   }
 }
 
